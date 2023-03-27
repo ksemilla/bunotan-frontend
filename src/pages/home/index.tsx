9efbc7b56326badb6dcce51ae461b561a3809212
Kevin Semilla <kevinsemilla@gmail.com>
2023-03-28 10:10:12 +1100

    Add postcss config file

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div style={{ fontSize: 20 }} className="text-gray-500 text-lg">this is home page</div>;
+  return <div className="text-3xl">this is home page</div>;
 };
 
 export default Home;
