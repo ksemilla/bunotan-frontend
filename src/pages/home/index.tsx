a646d2fcb45f0060b4f752408f53cf5a43ca14ad
kdmontero <kevindmontero@gmail.com>
2023-04-01 20:12:04 +0800

    created simple homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+
+
 const Home = () => {
-  return <div className="text-3xl">this is home page</div>;
+  return <>
+    <div className="bg-slate-400 h-window">
+      <div className="p-2 bg-slate-500 inline-block">
+        <h1 className="text-4xl">BUNOTAN</h1>
+      </div>
+      <div className="space-y-5 flex flex-col h-screen justify-center items-center">
+        <Link to="/register">
+          <button className="py-2 px-6 bg-blue-500 inline-block text-3xl">
+            Register
+          </button>
+        </Link>
+        <Link to="/login">
+          <div className="py-2 px-6 bg-blue-500 inline-block text-3xl">
+            Log In
+          </div>
+        </Link>
+      </div>
+    </div>
+  </>
 };
 
 export default Home;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,15 @@ import {
   Route,
 } from "react-router-dom";
 
-import { Account, Home, Jebaited, RoomDetail, RoomList } from "../pages";
+import { Account, Home, Register, Login, Jebaited, RoomDetail, RoomList } from "../pages";
 
 // You can do this:
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route path="" element={<Home />} />
+      <Route path="register" element={<Register />} />
+      <Route path="login" element={<Login />} />
       <Route path="my-account" element={<Account />} />
       <Route path="rooms" element={<Jebaited />}>
         <Route path="" element={<RoomList />} />
