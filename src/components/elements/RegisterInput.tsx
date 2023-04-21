f5bf3e5e11b30fb353028b81f7d3b768b7c540e6
kdmontero <kevindmontero@gmail.com>
2023-04-21 21:13:26 +0800

    placed ml-2 in div

--- src/components/elements/RegisterInput.tsx
+++ src/components/elements/RegisterInput.tsx
@@ -1,7 +1,9 @@
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const RegisterInput = (props: InputProps) => {
-    return <input {...props} className="rounded-lg p-2 ml-2" required />
+    return <div className="ml-2">
+        <input {...props} className="rounded-lg p-2" required />
+    </div>
 }
 
 export default RegisterInput
